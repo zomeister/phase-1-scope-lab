3d2781d1326bced7b4cdcb84d8a1c29d13bb22f1
Zoe Guidry <oyezoey@gmail.com>
2022-10-12 10:13:42 -0500

    commit tree 5e4e456ef5c6c329128753f0e10009dfb6003e75

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName= customerName.toUpperCase();
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCostumer = 'me';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCostumer = 'you';
+}
